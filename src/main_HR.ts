18251aa51b9faf6e92ec67cb0c7f77e9815f1927
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-12 22:19:50 +0200

    Update main_HR.ts

--- src/main_HR.ts
+++ src/main_HR.ts
@@ -5,59 +5,71 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
+let noteWebsite: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ', WA.player.tags);
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('clock').subscribe(closePopup);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
-    let noteWebsite: any;
-      
+
     WA.room.onEnterLayer("visibleNote").subscribe(async () => {
         console.log("Entering visibleNote layer");
 
-        noteWebsite = await WA.ui.website.open({
-            url: "src/note.html",
-            position: {
-                vertical: "top",
-                horizontal: "middle",
-            },
-            size: {
-                height: "30vh",
-                width: "50vw",
-            },
-            margin: {
-                top: "10vh",
-            },
-            allowApi: true,
-        });
-
+        try {
+            noteWebsite = await WA.ui.website.open({
+                url: "src/note.html",
+                position: {
+                    vertical: "top",
+                    horizontal: "middle",
+                },
+                size: {
+                    height: "30vh",
+                    width: "50vw",
+                },
+                margin: {
+                    top: "10vh",
+                },
+                allowApi: true,
+            });
+            console.log("noteWebsite opened:", noteWebsite);
+        } catch (error) {
+            console.error("Error opening noteWebsite:", error);
+        }
     });
-    
-   WA.room.onLeaveLayer("visibleNote").subscribe(async () => {
-        noteWebsite.close();
-    });
- 
 
-    function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+    WA.room.onLeaveLayer("visibleNote").subscribe(() => {
+        if (noteWebsite) {
+            console.log("Closing noteWebsite");
+            noteWebsite.close().then(() => {
+                console.log("noteWebsite closed");
+                noteWebsite = undefined;
+            }).catch(error => {
+                console.error("Error closing noteWebsite:", error);
+            });
+        } else {
+            console.log("noteWebsite is not defined");
+        }
+    });
+
+    function closePopup() {
+        if (currentPopup !== undefined) {
+            currentPopup.close();
+            currentPopup = undefined;
+        }
     }
-}
 }).catch(e => console.error(e));
-
 
 export {};
