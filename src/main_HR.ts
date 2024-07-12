ddbcc5864ffed2efb07f564ac55edde25c336657
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-12 22:14:09 +0200

    Update main_HR.ts

--- src/main_HR.ts
+++ src/main_HR.ts
@@ -5,7 +5,6 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
-let noteWebsite: any;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -24,7 +23,7 @@ WA.onInit().then(() => {
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
-
+    let noteWebsite: any;
       
     WA.room.onEnterLayer("visibleNote").subscribe(async () => {
         console.log("Entering visibleNote layer");
