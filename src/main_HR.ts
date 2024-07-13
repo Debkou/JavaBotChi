c4b9db43a2ef5eec04c3d5f235d2bce2f3ed3191
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-13 10:49:00 +0200

    Update main_HR.ts

--- src/main_HR.ts
+++ src/main_HR.ts
@@ -4,27 +4,7 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
-// Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
-
-    WA.room.area.onLeave('clock').subscribe(closePopup)
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
-
-    WA.onInit().then(() => {
+   WA.onInit().then(() => {
     console.log('Scripting API ready');
 
     let noteWebsite: any;
@@ -33,7 +13,7 @@ WA.onInit().then(() => {
         console.log("Entering visibleNote layer");
 
         noteWebsite = await WA.ui.website.open({
-            url: "./note.html",
+            url: "src/note.html",
             position: {
                 vertical: "top",
                 horizontal: "middle",
@@ -55,6 +35,27 @@ WA.onInit().then(() => {
     });
 
 }).catch(e => console.error(e));
+
+let currentPopup: any = undefined;
+
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+
+    WA.room.area.onEnter('clock').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    })
+
+    WA.room.area.onLeave('clock').subscribe(closePopup)
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
 
 
 }).catch(e => console.error(e));
